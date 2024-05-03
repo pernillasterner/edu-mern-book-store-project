af27db3c6a418de4701fedabd714a4002c732040
Pernilla Sterner <psterner@hotmail.com>
2024-05-03 16:52:58 +0200

    Install dotenv to handle sensitive info and mongoose to connect with db

--- backend/config.js
+++ backend/config.js
@@ -1,2 +1,10 @@
+import dotenv from "dotenv";
+
+// Läs .env-filen
+dotenv.config();
+
 // Defining ports
 export const PORT = 5555;
+
+// Get MongoDB URI from .env-filen
+export const mongoDBURL = "";
